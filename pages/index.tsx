45e0f831a1933ae06d715bc9e9a2adff7a02121f
dash7ou <fxfddfno@gmail.com>
2022-03-11 12:34:31 -0500

    auth part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,77 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next'
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import { useWeb3 } from '@3rdweb/hooks';
+import { client } from "../lib/sanityClient";
+import toast, {Toaster} from "react-hot-toast";
+
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
+
 
 const Home: NextPage = () => {
+  const { address, connectWallet } = useWeb3();
+
+  const welcomeUser = (userName:any, toastHandler = toast) => {
+    toastHandler.success(
+      `Welcome back${userName !== 'Unnamed' ? ` ${userName}` : ''}!`,
+      {
+        style: {
+          background: '#04111d',
+          color: '#fff',
+        },
+      }
+    )
+  }
+
+  useEffect(()=> {
+    if(!address) return;
+
+    (async ()=>{
+      try{
+        const userDoc = {
+          _type: "users",
+          _id: address,
+          userName: "Unnamed",
+          walletAddress: address
+        }
+
+        const result = await client.createIfNotExists(userDoc);
+        welcomeUser(result.userName)
+
+      }catch(err){
+        console.log(err)
+      }
+    })();
+
+  }, [address])
+
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className={style.wrapper}>
+      <Toaster position='top-center' reverseOrder={false}/>
+      {address ? (<>
+        <Header />
+        <Hero />
+      </>): (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={()=> connectWallet("injected")}
+          >
+            Connect Wallets
+          </button>
+          <div className={style.details}>
+            You need Chrome to be <br/> able to run this app.
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
